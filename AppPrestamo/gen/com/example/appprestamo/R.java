0f1d0f50cc80de601b1e78e977e422ceed7c4628
Fabricio António Romero Beltránd <fabriciobeltrand25@gmail.com>
2026-06-09 20:53:18 -0600

    Subiendo proyecto 2
    Conversor

--- AppPrestamo/gen/com/example/appprestamo/R.java
+++ AppPrestamo/gen/com/example/appprestamo/R.java
@@ -25,10 +25,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080008;
+        public static final int btnCalcular=0x7f080003;
+        public static final int btnLimpiar=0x7f080004;
         public static final int etCapital=0x7f080000;
+        public static final int etCuotaMensual=0x7f080007;
         public static final int etInteres=0x7f080001;
         public static final int etPlazo=0x7f080002;
+        public static final int etTotalInteres=0x7f080006;
+        public static final int etTotalPagar=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
